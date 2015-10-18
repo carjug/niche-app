34dbe3cdb316200b6d0d2923d8ce882e3469155a
Carly Jugler <carjug@gmail.com>
2015-10-17 18:01:00 -0700

    Adds user login manipulation

--- assets/javascripts/places.js
+++ assets/javascripts/places.js
@@ -7,6 +7,28 @@ $( function() {
 
   $("#register").click(function () {
     event.preventDefault();
+    var button    = $(this);
+    var method    = button.attr("method");
+    var name      = $("#user").val();
+    var pass      = $("#password").val();
+    var pass_conf = $("#password-confirmation").val();
+
+    var url  = "http://localhost:3000/register/" + name + "/" + pass + "/" + pass_conf
+
+    $.ajax(url, {
+      type: method,
+      dataType: 'json',
+      success: function (data, textStatus, jqHXR) {
+        if (jqHXR.status == 200) {
+          var user = data.username
+          showLoggedInState(user);
+
+        }
+        else if (jqHXR.status == 400) {
+          renderRegistration();
+        }
+      }
+    });
 
   });
 
@@ -32,7 +54,7 @@ $( function() {
           }
         }
         else if (jqHXR.status == 204) {
-          var apology = sendApology();
+          var apology = sendApology("place");
 
           messages.append(apology);
         }
@@ -46,9 +68,23 @@ function renderRegistration(element) {
   $(".sign-up-form").css("display", "block");
 }
 
-function sendApology() {
+function showLoggedInState(user) {
+  $(".sign-up-form").css("display", "none");
+  $("#sign-up").css("display", "none");
+  $(".login").toggleClass("logout")
+  $(".logout").text("Log Out")
+
+  $("header").slideDown();
+}
+
+function sendApology(thing) {
   var p = $("<p></p>");
-  var apology = "Sorry! There are no places in our database for that city."
+  if(thing == "place") {
+    var apology = "Sorry! There are no places in our database for that city.";
+  }
+  else if(thing == "user") {
+    var apology = "User could not be created."
+  }
 
   p.html(apology)
 
