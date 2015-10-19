68c1985bbeb1a19110cc975f904d8df897a5126b
Carly Jugler <carjug@gmail.com>
2015-10-19 13:40:01 -0700

    Adds greeting scope to home controller

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -3,15 +3,19 @@
 
   angular
     .module('app')
-    .controller('HomeController', HomeController);
+    .controller('HomeController', HomeController)
 
-  HomeController.$inject = ['UserService', '$rootScope'];
+  HomeController.$inject = ['UserService', '$rootScope', '$scope'];
 
-  function HomeController(UserService, $rootScope) {
+
+  function HomeController(UserService, $rootScope, $scope) {
     var vm  = this;
     vm.user = $rootScope.globals.currentUser.username || null;
 
-    $scope.user = vm.user
+    $scope.user = vm.user;
+    $scope.greeting = "Welcome, " + $scope.user + "!";
+  }
+
   //   vm.deleteUser = deleteUser;
 
     // initController();
@@ -33,6 +37,5 @@
   //       loadAllUsers();
   //     });
   //   }
-  }
 
 })();
